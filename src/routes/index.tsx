9c877f973b7c9f14b6d550d4820dd29610a937da
Breno Christovam Ramos <breno.christovam@iesb.edu.br>
2023-09-29 10:15:18 -0300

    Atualização do index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ import Login from "../screens/login";
 import HomePage from "../screens/homepage";
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import OrderList from "../screens/orderlist";
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 type RootStackParamsList = {
@@ -28,19 +28,23 @@ declare global {
 const HomeStack = createNativeStackNavigator();
 const HomeStackScreen = () => (
     <HomeStack.Navigator>
-        <HomeStack.Screen name="HomeScreen" component={HomePage} />       
+        <HomeStack.Screen name="HomeScreen" component={HomePage} options={{
+            headerTitle: '',
+            headerTintColor: 'white',
+            headerTransparent: true,
+        }} />       
     </HomeStack.Navigator>
 );
-// <HomeStack.Screen name="ProductDetails" component={ProductDetailsPage} />
+// <HomeStack.Screen name="ProductDetails" component={ProductDetailsPage} /
 
-const OrderStack = createNativeStackNavigator
+const OrderStack = createNativeStackNavigator();
 const OrderStackScreen = () => (
     <OrderStack.Navigator>
         <OrderStack.Screen name="OrderListScreen" component={OrderList} />
     </OrderStack.Navigator>
 );
 
-const SettingStack = createNativeStackNavigator
+const SettingStack = createNativeStackNavigator();
 const SettingStackScreen = () => (
     <SettingStack.Navigator>
         <SettingStack.Screen name="OrderListScreen" component={OrderList} />
@@ -49,13 +53,37 @@ const SettingStackScreen = () => (
 
 const Tab = createBottomTabNavigator();
 
+const TabBarIcon = props => {
+    const {focused, name, color, size} = props;
+
+    let iconName;
+
+    if (name === 'HomePage') {
+    iconName = focused ? 'home' : 'home-outline';
+} else if (name === 'Compras') {
+    iconName = focused ? 'list' : 'list-outline';
+} else if (name === 'Ajustes') {
+    iconName = focused ? 'settings' : 'settings-outline';
+}
+
+return <Ionicons name={iconName} size={size} color={color} />
+}
+
 const Routes = () => (
     <NavigationContainer>
-        <Tab.Navigator>
-            <Tab.Screen name="HomePage" component={HomeStackScreen}/>
-            <Tab.Screen name="Compras" component={OrderStackScreen}/>
-            <Tab.Screen name="Ajustes" component={SettingStackScreen}/>
+        <Tab.Navigator
+            screenOptions={({route}) => ({
+            headerShown: false,
+            tabBarIcon: props => <TabBarIcon {...props} name={route.name} />,
+            tabBarActiveTintColor: '#62C567',
+            tabBarInactiveTintColor: 'gray',
+            })}>
+
+            <Tab.Screen name="HomePage" component={HomeStackScreen} />
+            <Tab.Screen name="Compras" component={OrderStackScreen} />
+            <Tab.Screen name="Ajustes" component={HomeStackScreen} />
         </Tab.Navigator>
+
         {/*<Stack.Navigator initialRouteName="Home">
             <Stack.Screen name= "Home" component={Home} />
             <Stack.Screen name= "Webnavigator" component={Webnavigator} />
